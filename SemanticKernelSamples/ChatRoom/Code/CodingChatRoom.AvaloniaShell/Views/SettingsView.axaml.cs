71e3bdb10c8fa9c6fd9870c907a90d7262729959
lindexi <lindexi_gd@outlook.com>
2026-09-04 20:10:19 +0800

    设置提供安装工具

--- SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell/Views/SettingsView.axaml.cs
+++ SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell/Views/SettingsView.axaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -24,6 +26,25 @@ public partial class SettingsView : UserControl
             && viewModel.TestWindowsSandboxConnectionCommand.CanExecute(null))
         {
             viewModel.TestWindowsSandboxConnectionCommand.Execute(null);
+        }
+    }
+
+    private async void OnInstallOrUpdateLanguageServerClick(object? sender, RoutedEventArgs e)
+    {
+        var startInfo = new ProcessStartInfo("dotnet")
+        {
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        startInfo.ArgumentList.Add("tool");
+        startInfo.ArgumentList.Add("update");
+        startInfo.ArgumentList.Add("-g");
+        startInfo.ArgumentList.Add("roslyn-language-server");
+
+        using Process? process = Process.Start(startInfo);
+        if (process is not null)
+        {
+            await process.WaitForExitAsync();
         }
     }
 }
