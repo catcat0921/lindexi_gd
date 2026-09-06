a96010dce945077c39e92295c340f51bfbb9bb7b
lindexi <lindexi_gd@163.com>
2026-09-06 11:03:26 +0800

    Revert "优化对工作路径界面的设置"
    This reverts commit 5890e72e0004ab61dbc87b4670ae9766ee1b6e79.

--- SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell.Tests/ChatViewModelTests.cs
+++ SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell.Tests/ChatViewModelTests.cs
@@ -475,7 +475,7 @@ public sealed class ChatViewModelTests
         await WaitUntilAsync(() => viewModel.CommittedWorkspacePath is not null);
 
         Assert.AreEqual(Path.GetFullPath(workspacePath), viewModel.CommittedWorkspacePath);
-        Assert.AreEqual($"工作路径：{Path.GetFullPath(workspacePath)}", viewModel.WorkspaceStatusText);
+        StringAssert.Contains(viewModel.WorkspaceStatusText, "已设置");
         Assert.IsTrue(viewModel.CanApplyWorkspace);
     }
 
